cb3fcaf9a97a875718cd1f3297c1eca2d0f482a3
SanoKum <taylor410san@gmail.com>
2024-01-12 08:43:54 +0900

    c3

--- case/09.Taylor-Green/plot_taylorGreen.py
+++ case/09.Taylor-Green/plot_taylorGreen.py
@@ -9,7 +9,10 @@ import matplotlib.pyplot as plt
 with open("log", "r") as f:
 	lines = f.read().splitlines()
 
-nLoop = 3
+nLoop = 4
+
+
+M0=0.4
 
 time=[]
 ros =[]
@@ -17,7 +20,7 @@ rok =[]
 
 for line in lines:
 	if "Total Time" in line:
-		time.append(float(line.split(" ")[-1]))
+		time.append(M0*float(line.split(" ")[-1]))
 
 	if "ros" in line:
 		ros.append(float(line.split(" ")[-1]))
@@ -35,21 +38,25 @@ rok4 = (rok3)/rok3[0]
 
 # plot
 
+
 fig = plt.figure(figsize=(12,6))
 
 ax1 = fig.add_subplot(122)
-ax1.plot(time[0:len(ros4)], ros4)
+ax1.plot(time[0:len(ros4)], ros4, linewidth=1, color='r')
 ax1.set_xlabel("Time [s]")
 ax1.set_ylabel("(del ros/init ros) [-]")
 ax1.set_xlim([0,200])
-ax1.set_ylim([-0.02,0.005])
+ax1.set_ylim([-0.04,0.01])
+ax1.hlines(0.0, 0, 200, color='black', linestyles='dotted')
 
 ax2 = fig.add_subplot(121)
-ax2.plot(time[0:len(rok4)], rok4)
+ax2.plot(time[0:len(rok4)], rok4, linewidth=1, color='r')
 ax2.set_xlabel("Time [s]")
 ax2.set_ylabel("(rok/init rok) [-]")
 ax2.set_xlim([0,200])
-ax2.set_ylim([0.5,2.5])
+ax2.set_ylim([0.6,1.6])
+
+ax2.hlines(1.0, 0, 200, color='black',linestyles='dotted')
 
 plt.show()
 
--- case/09.Taylor-Green/run_keep/plot_taylorGreen.py
+++ case/09.Taylor-Green/run_keep/plot_taylorGreen.py
@@ -6,10 +6,13 @@ import matplotlib.pyplot as plt
 #		if "ros" in line:
 #			print(line.rstrip("\n"))
 
-with open("log", "r") as f:
+#4th
+with open("log_4th", "r") as f:
 	lines = f.read().splitlines()
 
-nLoop = 3
+nLoop = 4
+
+M0=0.4
 
 time=[]
 ros =[]
@@ -17,7 +20,7 @@ rok =[]
 
 for line in lines:
 	if "Total Time" in line:
-		time.append(float(line.split(" ")[-1]))
+		time.append(M0*float(line.split(" ")[-1]))
 
 	if "ros" in line:
 		ros.append(float(line.split(" ")[-1]))
@@ -35,22 +38,131 @@ rok4 = (rok3)/rok3[0]
 
 # plot
 
+
 fig = plt.figure(figsize=(12,6))
 
 ax1 = fig.add_subplot(122)
-ax1.plot(time[0:len(ros4)], ros4)
+ax1.plot(time[0:len(ros4)], ros4, linewidth=1.0, color='g', label="4th")
 ax1.set_xlabel("Time [s]")
 ax1.set_ylabel("(del ros/init ros) [-]")
 ax1.set_xlim([0,200])
-ax1.set_ylim([-0.02,0.005])
+ax1.set_xticks( np.arange(0, 201, 20))
+ax1.set_ylim([-0.04,0.01])
+ax1.hlines(0.0, 0, 200, color='black', linestyles='dotted')
 
 ax2 = fig.add_subplot(121)
-ax2.plot(time[0:len(rok4)], rok4)
+ax2.plot(time[0:len(rok4)], rok4, linewidth=1.0, color='g', label="4th")
 ax2.set_xlabel("Time [s]")
 ax2.set_ylabel("(rok/init rok) [-]")
 ax2.set_xlim([0,200])
-ax2.set_ylim([0.5,2.5])
+ax2.set_ylim([0.6,1.6])
+ax2.set_xticks( np.arange(0, 201, 20))
 
+ax2.hlines(1.0, 0, 200, color='black',linestyles='dotted')
+
+
+#3rd
+with open("log_3rd", "r") as f:
+	lines = f.read().splitlines()
+
+nLoop = 3
+
+M0=0.4
+
+time=[]
+ros =[]
+rok =[]
+
+for line in lines:
+	if "Total Time" in line:
+		time.append(M0*float(line.split(" ")[-1]))
+
+	if "ros" in line:
+		ros.append(float(line.split(" ")[-1]))
+
+	if "rok" in line:
+		rok.append(float(line.split(" ")[-1]))
+
+ros2 = np.array(ros)
+ros3 = ros2[nLoop-1::nLoop]
+ros4 = (ros3-ros3[0])/ros3[0]
+
+rok2 = np.array(rok)
+rok3 = rok2[nLoop-1::nLoop]
+rok4 = (rok3)/rok3[0]
+
+# plot
+
+
+ax1.plot(time[0:len(ros4)], ros4, linewidth=1.0, color='b', label="3rd TVD")
+#ax1.set_xlabel("Time [s]")
+#ax1.set_ylabel("(del ros/init ros) [-]")
+#ax1.set_xlim([0,200])
+#ax1.set_ylim([-0.04,0.01])
+#ax1.hlines(0.0, 0, 200, color='black', linestyles='dotted')
+
+ax2.plot(time[0:len(rok4)], rok4, linewidth=1.0, color='b', label="3rd TVD")
+#ax2.set_xlabel("Time [s]")
+#ax2.set_ylabel("(rok/init rok) [-]")
+#ax2.set_xlim([0,200])
+#ax2.set_ylim([0.6,1.6])
+
+#ax2.hlines(1.0, 0, 200, color='black',linestyles='dotted')
+
+
+#4th redo
+with open("log_4th_fixDT", "r") as f:
+	lines = f.read().splitlines()
+
+
+nLoop = 4
+
+M0=0.4
+
+time=[]
+ros =[]
+rok =[]
+
+for line in lines:
+	if "Total Time" in line:
+		time.append(M0*float(line.split(" ")[-1]))
+
+	if "ros" in line:
+		ros.append(float(line.split(" ")[-1]))
+
+	if "rok" in line:
+		rok.append(float(line.split(" ")[-1]))
+
+ros2 = np.array(ros)
+ros3 = ros2[nLoop-1::nLoop]
+ros4 = (ros3-ros3[0])/ros3[0]
+
+rok2 = np.array(rok)
+rok3 = rok2[nLoop-1::nLoop]
+rok4 = (rok3)/rok3[0]
+
+# plot
+
+
+ax1.plot(time[0:len(ros4)], ros4, linewidth=1, color='r')
+#ax1.set_xlabel("Time [s]")
+#ax1.set_ylabel("(del ros/init ros) [-]")
+#ax1.set_xlim([0,200])
+#ax1.set_ylim([-0.04,0.01])
+#ax1.hlines(0.0, 0, 200, color='black', linestyles='dotted')
+
+ax2.plot(time[0:len(rok4)], rok4, linewidth=1, color='r')
+#ax2.set_xlabel("Time [s]")
+#ax2.set_ylabel("(rok/init rok) [-]")
+#ax2.set_xlim([0,200])
+#ax2.set_ylim([0.6,1.6])
+
+#ax2.hlines(1.0, 0, 200, color='black',linestyles='dotted')
+
+
+
+
+plt.legend()
 plt.show()
 
 
